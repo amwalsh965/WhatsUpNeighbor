799f73bb904ada6388cabcc84200f8ef010a6109
amwalsh965 <amwalsh965@gmail.com>
2026-01-29 10:06:14 -0500

    Project Setup

---
+++ whatsupneighbor/whatsupneighbor/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for whatsupneighbor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsupneighbor.settings')
+
+application = get_wsgi_application()
