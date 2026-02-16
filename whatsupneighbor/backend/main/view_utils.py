facdea02449d855218aba6e641cf4c62a87842d5
amwalsh965 <amwalsh965@gmail.com>
2026-02-15 23:30:55 -0500

    App.jsx basic frontend and frontend backend connection

--- whatsupneighbor/backend/main/view_utils.py
+++ whatsupneighbor/backend/main/view_utils.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.utils import timezone
 from .models import *
 
@@ -29,39 +30,6 @@ class TransactionViews:
         self.transaction = Transaction.objects.get(pk=pk)
 
 
-class UserViews:
-    def __init__(self):
-        self.user = None
-
-    def create_user(self, f_name, l_name, photo_url, user_bio):
-        self.user = User.objects.create()
-
-    def get_user(self, pk):
-        self.user = User.objects.get(pk=pk)
-
-    def update_user(self, **kwargs):
-        ALLOWED_FIELDS = [
-            "f_name",
-            "l_name",
-            "address",
-            "photo_url",
-            "user_bio",
-            "role",
-            "neighborhood",
-        ]
-
-        for field, value in kwargs:
-            if field is not None and field in ALLOWED_FIELDS:
-                if field == "role" and (value != "admin" or value != "neighbor"):
-                    return {"error", "invalid role keyword"}
-                setattr(self.user, field, value)
-        self.user.save()
-        return self.user
-
-    def delete_user(self, user_id):
-        User.objects.delete(user_id)
-
-
 class TrustFeedbackViews:
     def __init__(self, pk: int):
         self.tfv = TrustFeedback.objects.get(pk=pk)
@@ -69,7 +37,6 @@ class TrustFeedbackViews:
         self.lender: User = self.tfv.lender
         self.borrower: User = self.tfv.borrower
 
-    # Calculating user trust fields for a user
     def calc_trust_fields(self, user: User):
         trust_objs_borrowing = TrustFeedback.objects.filter(borrower_id=user.pk)
         trust_objs_lending = TrustFeedback.objects.filter(lender_id=user.pk)
@@ -96,7 +63,68 @@ class TrustFeedbackViews:
 
 
 class ListingViews:
-    pass
+    def __init__(self, pk: int):
+        self.listing = Listing.objects.get(pk=pk)
+
+    def create_listing(
+        self,
+        owner: User,
+        title: str,
+        listing_bio: str,
+        photo_url: str,
+        listing_type: str = Listing.Type.REQUEST,
+        item: Item = None,
+        skill: Skill = None,
+        status: str = Status.OPEN,
+    ):
+        new_listing = Listing.objects.create(
+            user=owner,
+            title=title,
+            listing_bio=listing_bio,
+            image_url=photo_url,
+            type=listing_type,
+            item=item,
+            skill=skill,
+            start_date=timezone.now(),
+            end_date=timezone.now()
+            + timedelta(days=7),  # end dates are defaulted to 7 days out
+            status=status,
+            neighborhood=owner.neighborhood,
+        )
+        new_listing.save()
+        return new_listing
+
+    def update_listing(
+        self,
+        title: str = None,
+        listing_bio: str = None,
+        photo_url: str = None,
+        listing_type: str = None,
+        status: str = None,
+        item: Item = None,
+        skill: Skill = None,
+    ):
+        if title:
+            self.listing.title = title
+        if listing_bio:
+            self.listing.listing_bio = listing_bio
+        if photo_url:
+            self.listing.image_url = photo_url
+        if listing_type:
+            self.listing.type = listing_type
+        if status:
+            self.listing.status = status
+        if item is not None:
+            self.listing.item = item
+        if skill is not None:
+            self.listing.skill = skill
+
+        self.listing.save()
+        return self.listing
+
+    def delete_listing(self):
+        self.listing.delete()
+        return True
 
 
 class NeighborhoodViews:
--- whatsupneighbor/frontend/src/App.jsx
+++ whatsupneighbor/frontend/src/App.jsx
@@ -3,16 +3,171 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [number, setNumber] = useState(null)
+const URL_BASE = "http://127.0.0.1:8000/main/"
 
-  useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/example/').then(res => res.json()).then(data => setNumber(data.number))
-  }, [])
+function App() {
+  const [number, setNumber] = useState(null);
+  const [userId, setUserId] = useState("");
+  const [profileId, setProfileId] = useState("");
+  const [results, setResults] = useState([]);
+  const [feedbackId, setFeedbackId] = useState("");
+  const [trustFeedback, setTrustFeedback] = useState(null);
+
+  const [trustData, setTrustData] = useState({
+    transaction: "",
+    borrower: "",
+    lender: "",
+    item_returned: "",
+    return_timeliness: "",
+    item_condition: "",
+    rating_score: "",
+    timestamp: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target
+    setTrustData({
+      ...trustData, [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const FEEDBACK_BASE = URL_BASE + "trust_feedback/"
+
+  //POST
+  const createFeedback = async () => {
+    const res = await fetch(FEEDBACK_BASE, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        ...trustData,
+        rating_score: trustData.rating_score  ? parseInt(trustData.rating_score) : null,
+      }),
+    });
+
+    const data = await res.json();
+    alert("Feedback Id: " + data.id);
+  }
+
+  //GET
+
+  const getFeedback = async () => {
+    const res = await fetch(`${FEEDBACK_BASE}${feedbackId}/`);
+    const data = await res.json();
+    setTrustFeedback(data);
+  }
+
+  const getAllFeedback = async () => {
+    const res = await fetch(FEEDBACK_BASE);
+    const data = await res.json();
+    setResults(data);
+  }
+
+  const filterByBorrower = async () => {
+    const res = await fetch(`${FEEDBACK_BASE}?borrower_id=${trustData.borrower}`);
+    const data = await res.json();
+    setResults(data);
+  }
+
+  const filterByLender = async () => {
+    const res = await fetch(`${FEEDBACK_BASE}?lender_id=${trustData.lender}`);
+    const data = await res.json();
+    setResults(data);
+  }
+
+  //PUT
+  const updateFeedback = async () => {
+    const res = await fetch(`${FEEDBACK_BASE}${feedbackId}/`, {
+      method: "PUT",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(trustData),
+    });
+
+    const data = await res.json();
+    setTrustFeedback(data);
+    alert("Updated");
+  }
 
   return (
-    <h1>Number: {number}</h1>
-  )
+    <div style={{ padding: "40px" }}>
+      <h1> Trust Feedback Manager </h1>
+      <h2> CREATE </h2>
+      <input name="borrower" placeholder="Borrower ID" onChange={handleChange} />
+      <br />
+
+      <input name="lender" placeholder="Lender ID" onChange={handleChange} />
+      <br />
+
+      <input name="transaction" placeholder="Transaction ID" onChange={handleChange} />
+      <br />
+
+      <label>
+        Item Returned:
+        <input type="checkbox" name="item_returned" onChange={handleChange} />
+      </label>
+      <br />
+
+      <label>
+        Return Timeliness:
+        <select name = "return_timeliness" onChange={handleChange}>
+          <option value="on_time">On Time</option>
+          <option value="late">Late</option>
+        </select>
+      </label>
+      <br />
+
+      <label>
+        Item Condition:
+        <select name = "item_condition" onChange={handleChange}>
+          <option value="good">Good</option>
+          <option value="damaged">Damaged</option>
+        </select>
+      </label>
+      <br />
+
+      <input name="rating_score" placeholder="Rating Score" onChange={handleChange} />
+      <br /> <br />
+
+      <button onClick={createFeedback}>✨Create Feedback</button>
+
+      <hr />
+
+      <h2> GET / UPDATE </h2>
+
+      <input placeholder='Feedback ID' value={feedbackId} onChange={(e) => setFeedbackId(e.target.value)} />
+
+      <button onClick={getFeedback}>1️⃣ Get One</button>
+      <button onClick={updateFeedback}>♻️ Update</button>
+      <button onClick={getAllFeedback}>➕ Get All</button>
+      <button onClick={filterByBorrower}>🔍Filter by Borrower</button>
+      <button onClick={filterByLender}>🔎 Filter by Lender</button>
+
+      <hr />
+
+      {trustFeedback && (
+        <div>
+          <h3>Single Feedback</h3>
+          <pre>{JSON.stringify(trustFeedback, null, 2)}</pre>
+        </div>
+      )}
+
+      {results.length > 0 && (
+        <div><h3>Results</h3><pre>{JSON.stringify(results,null,2)}</pre></div>
+      )}
+
+    </div>
+  );
+
+  
 }
 
-export default App
+
+//   useEffect(() => {
+//     fetch('http://127.0.0.1:8000/api/example/').then(res => res.json()).then(data => setNumber(data.number))
+//   }, [])
+
+//   return (
+//     <h1>Number: {number}</h1>
+//   )
+// }
+
+export default App;
