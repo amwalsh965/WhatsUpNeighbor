142e9a9c971987a992a723fb50b382ca028079c0
amwalsh965 <amwalsh965@gmail.com>
2026-02-22 22:39:23 -0500

    Resolved merge issues

--- whatsupneighbor/backend/main/urls.py
+++ whatsupneighbor/backend/main/urls.py
@@ -6,13 +6,9 @@ app_name = "main"
 # When a view is created, a url path must also be created. If you want a specific endpoint, specify it in the ""
 
 urlpatterns = [
-    path("test/", views.test, name="test"),
-
     path("user/", views.user_views),
     path("user/<int:user_id>/", views.user_views),
-
     path("user/<int:user_id>/profile/", views.user_profile, name="user-profile"),
-
     path("trust_feedback/", views.trust_feedback_collection),
     path("trust_feedback/<int:trust_feedback_id>/", views.trust_feedback_details),
     # path("listing/", views.listing_views),
